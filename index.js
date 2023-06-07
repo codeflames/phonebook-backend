1573334ef48246f79574feb54797cad2b7b377fd
codeflames <yerekadonald@gmail.com>
2023-06-07 20:01:57 +0100

    phonebook database connect with frontend

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
 
 const app = express();
+const Person = require('./models/persons');
+
 
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body); });
@@ -16,59 +19,72 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 
 
-let entries = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
+// let entries = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ];
 
 // Info about the phonebook entries
 app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${entries.length} people</p>
-    <p>${new Date()}</p>`
-    );
+  Person.find({}).then(persons => {
+    response.send(
+      `<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+      );
+  });
 }
 );
 
 // Get all entries
 app.get('/api/persons', (request, response) => {
-  response.json(entries);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
 
 // Get a single entry
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const entry = entries.find(entry => entry.id === id);
-  if (entry) {
-    response.json(entry);
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id).then(person => {
+    
+  if (person) {
+    response.json(person);
   } else {
     response.status(404).end();
   }
+})
+.catch(error => {
+  next(error);
 });
+});
+  
 
 // Delete a single entry
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  entries = entries.filter(entry => entry.id !== id);
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id;
+  Person.findByIdAndRemove(id).then(result => {
+
   response.status(204).end();
+  })
+  .catch(error => next(error));
 });
 
 // Generate a random id using Math.random
@@ -90,21 +106,59 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({
       error: 'number missing'
     });
-  } else if (entries.find(entry => entry.name === body.name)) {
-    return response.status(400).json({
-      error: 'name already exist in the server: name must be unique'
-    });
   }
-  const entry = {
-    id: generateId(),
+  //  else if (entries.find(entry => entry.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: 'name already exist in the server: name must be unique'
+  //   });
+  // }
+  const entry = new Person({
     name: body.name,
     number: body.number
-  };
-  entries = entries.concat(entry);
-  response.json(entry);
+  });
+
+  entry.save(entry).then(savedEntry => {
+    response.json(savedEntry);
+  }).catch(error => console.log(error));
+
 
   morgan.token('body', function (req, res) { return JSON.stringify(req.body); });
 });
+
+// Update an entry
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+  const id = request.params.id;
+  const entry = {
+    name: body.name,
+    number: body.number
+  };
+  Person.findByIdAndUpdate(id, entry, { new: true })
+    .then(updatedEntry => {
+      response.json(updatedEntry);
+    })
+    .catch(error => next(error));
+});
+
+
+// unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
+
+//error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
+
+  
 
 
 
